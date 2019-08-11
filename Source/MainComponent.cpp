e342f1035360dfcd1eb7ac8ec1b5a3aac64e41ee
Azrul Saleh <azrulsaleh@me.com>
2019-08-10 23:37:00 -0700

    Circular motion with spine path

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -14,7 +14,8 @@ MainComponent::MainComponent()
     // Make sure you set the size of the component after
     // you add any child components.
     setSize (800, 600);
-    setFramesPerSecond (60); // This sets the frequency of the update calls.
+    setFramesPerSecond (60);
+        //setFramesPerSecond (24);
 }
 
 MainComponent::~MainComponent()
@@ -33,11 +34,27 @@ void MainComponent::paint (Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
+    //Circular motion
+        //g.setColour (getLookAndFeel().findColour (Slider::thumbColourId));
+        //int radius = 150;
+        //Point<float> p ((getWidth()  / 2.0f) + (1.0f * radius) * std::sin (getFrameCounter() * 0.04f),
+                        //(getHeight() / 2.0f) + (1.0f * radius) * std::cos (getFrameCounter() * 0.04f));
+        //g.fillEllipse(p.x, p.y, 30.0f, 30.0f);
+    
     g.setColour (getLookAndFeel().findColour (Slider::thumbColourId));
-    int radius = 150;
-    Point<float> p ((getWidth()  / 2.0f) + (1.0f * radius) * std::sin (getFrameCounter() * 0.04f),
-                    (getHeight() / 2.0f) + (1.0f * radius) * std::cos (getFrameCounter() * 0.04f));
-    g.fillEllipse(p.x, p.y, 30.0f, 30.0f);
+    auto numberOfDots = 15;
+    Path spinePath;
+    for (auto i = 0; i < numberOfDots; ++i)
+    {
+        int radius = 150;
+        Point<float> p ((getWidth()  / 2.0f) + (1.0f * radius) * std::sin ((getFrameCounter() * 0.04f) + (i * 0.12f)),
+                        (getHeight() / 2.0f) + (1.0f * radius) * std::cos ((getFrameCounter() * 0.04f) + (i * 0.12f)));
+        if (i == 0)
+            spinePath.startNewSubPath(p);
+        else
+            spinePath.lineTo(p);
+    }
+    g.strokePath(spinePath, PathStrokeType(4.0f));
 }
 
 void MainComponent::resized()
